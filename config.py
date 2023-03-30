4c26f209c4143509764b9e9d81a96b5e4f33a3a9
Daniel Ong <daniel.ong@snowflake.com>
2023-03-31 00:45:41 +0800

    Conda

---
+++ config.py
@@ -0,0 +1,8 @@
+snowflake_conn_prop = {
+   "account": "ws90724.ap-southeast-1",
+   "user": "admin",
+   "role": "ACCOUNTADMIN",
+   "database": "AIRLINE",
+   "schema": "PUBLIC",
+   "warehouse": "DATA_SCIENCE_AIRLINE_WH",
+}
